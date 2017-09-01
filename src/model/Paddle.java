14b738d6e08deddfcb0410d5ccc88639c70e1130
lebentle <lebentle@uwaterloo.ca>
2017-09-01 16:44:47 -0700

    The Paddle now displays to the screen

--- src/model/Paddle.java
+++ src/model/Paddle.java
@@ -11,15 +11,15 @@ import java.awt.*;
 // handle key left, and handle key right in game
 
 public class Paddle extends Sprite {
-    public static final int WIDTH = 50;
-    public static final int HEIGHT = 10;
-    public static final int X_OFFSET =  Game.WIDTH / 2;
-    public static final int Y_OFFSET = 50;
-    public static final int PADDLE_SPEED = 2;
-    public static final Color COLOR = new Color(0, 0, 0);
+    protected static final int WIDTH = 50;
+    protected static final int HEIGHT = 10;
+    protected static final int X_OFFSET =  Game.WIDTH / 2;
+    protected static final int Y_OFFSET = 550;
+    protected static final int PADDLE_SPEED = 2;
+    protected static final Color COLOR = new Color(250, 250, 250);
 
     public Paddle(){
-        super(WIDTH,HEIGHT,X_OFFSET,Y_OFFSET);
+        super(X_OFFSET,Y_OFFSET, WIDTH, HEIGHT);
     }
 
     protected void handleBoundary_x() {
@@ -44,20 +44,10 @@ public class Paddle extends Sprite {
     }
 
     public void draw(Graphics g){
-        Polygon Paddle = createpaddle();
         g.setColor(COLOR);
-        g.fillPolygon(Paddle);
+        g.fillRect(X_OFFSET, Y_OFFSET, WIDTH, HEIGHT);
         return;
     }
 
-
-    public Polygon createpaddle(){
-        Polygon paddle = new Polygon();
-        paddle.addPoint(0,0);
-        paddle.addPoint(0, HEIGHT);
-        paddle.addPoint(WIDTH,0);
-        paddle.addPoint(WIDTH, HEIGHT);
-        return paddle;
-    }
 
 }
